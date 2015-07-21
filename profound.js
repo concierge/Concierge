4b966c4ea10036c4454e5a7912f06a964b8e8494
Jay Harris <jay.harris@outlook.co.nz>
2015-07-21 15:02:53 +1200

    Fixed bugs for PR

--- profound.js
+++ profound.js
@@ -16,12 +16,17 @@ exports.help = function() {
 
 exports.fetch = function(callback) {
     request.post('http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence', function(error, response) {
+        if (error || response.statusCode !== 200) {
+            callback("Something went horribly, terribly wrong. It's probably your fault.");
+            return;
+        }
+
         callback(response.body);
     }); 
 };
 
 exports.run = function(api, event) {
-    exports.fetch(function (sentence){
+    exports.fetch(function (sentence) {
         api.sendMessage(sentence, event.thread_id);
     });
 };
