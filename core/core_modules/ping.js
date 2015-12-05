828388c55aa509b2cff4a5885d933d8182e19512
Matthew <mrk45@uclive.ac.nz>
2015-12-05 13:48:39 +1300

    update ping and reddit

--- core/core_modules/ping.js
+++ core/core_modules/ping.js
@@ -1,10 +1,22 @@
+/**
+ * A command that can check if Kassy is alive.
+ *
+ * Written By:
+ * 		Matthew Knox
+ *
+ * License:
+ *		MIT License. All code unless otherwise specified is
+ *		Copyright (c) Matthew Knox and Contributors 2015.
+ */
+
 var os = require("os");
 
 exports.match = function(text) {
-  return text === this.commandPrefix + 'ping';
+    return text === this.commandPrefix + 'ping';
 };
 
 exports.run = function(api, event) {
-  api.sendMessage(this.packageInfo.name + ' ' + this.packageInfo.version + ' @ ' + os.hostname(), event.thread_id);
-  return false;
+    api.sendMessage(this.packageInfo.name + ' ' + this.packageInfo.version + ' @ ' + os.hostname()
+      + ' (' + os.type() + ' ' + os.arch() + ')', event.thread_id);
+    return false;
 };
--- modules/common/reddit.js
+++ modules/common/reddit.js
@@ -1,10 +1,9 @@
 var request = require.safe('request'),
     reddit = require.safe('redwrap');
 
-
 exports.reddit = function(thing, numberOfQueries, callback) {
     reddit.r(thing).limit(numberOfQueries, function(err, req, res){
-        if (!err || req !== null || req !== undefined) {
+        if (!err && req && req.data) {
             callback(false, req.data.children);
         }
         else {
