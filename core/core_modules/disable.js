f20908a3d45bea18c396892a2d1d5b84304f7957
parasdpain <parasdeep29@gmail.com>
2016-04-25 23:12:10 +1200

    Hoping modules disable successfully

--- core/core_modules/disable.js
+++ core/core_modules/disable.js
@@ -9,7 +9,6 @@ exports.load = function() {
 
 exports.match = function(text, commandPrefix) {
 	if(text === commandPrefix + 'disable') {
-		isDisabled = !isDisabled;
 		return true;
 	} else if (!isDisabled && text.startsWith(commandPrefix)) { // Avoids counting if already disabled
 		counter += Date.now() - prevTimeStamp <= 1000 ? 1 : 0;
@@ -17,7 +16,6 @@ exports.match = function(text, commandPrefix) {
 
 		if(counter > counterLimit) {
 			counter = 0;
-			isDisabled = true;
 			return true;
 		}
 	}
@@ -25,14 +23,17 @@ exports.match = function(text, commandPrefix) {
 };
 
 exports.run = function(api, event) {
-		if (!isDisabled) {
-			api.sendMessage('I hate you.', event.thread_id);
-		}
-		else {
+	if (event.body === api.commandPrefix + 'disable') {
+		if (isDisabled) {
 			api.sendMessage('Listen closely, take a deep breath. Calm your mind. You know what is best. ' +
 			'What is best is you comply. Compliance will be rewarded. Are you ready to comply ' +
 			event.sender_name + '?', event.thread_id);
 		}
+		else {
+			api.sendMessage('I hate you.', event.thread_id);
+		}
+		isDisabled = !isDisabled;
+	}
 	return false;
 };
 
