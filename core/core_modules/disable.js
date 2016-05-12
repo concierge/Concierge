786c711238d8f2e8d08c49e5bd813b0c953a52b6
parasdpain <parasdeep29@gmail.com>
2016-05-13 01:04:03 +1200

    Using JSBeautify

--- core/core_modules/disable.js
+++ core/core_modules/disable.js
@@ -16,7 +16,7 @@ var messages = [
     'Hmm, maybe I need to reconsider the meaning of spam.'
 ];
 
-exports.load = function() {
+exports.load = function () {
     if (!exports.config) {
         exports.config = {};
     }
@@ -25,7 +25,7 @@ exports.load = function() {
     prevTimeStamp = Date.now();
 };
 
-exports.match = function(event, commandPrefix) {
+exports.match = function (event, commandPrefix) {
     // Add disabled flag for thread if it doesn't already exists
     if (!threads[event.thread_id]) {
         threads[event.thread_id].isThreadDisabled = false;
@@ -40,21 +40,21 @@ exports.match = function(event, commandPrefix) {
         threads[event.thread_id].msgIndexDisable = 1;
         return true;
     } else if (!threads[event.thread_id].isThreadDisabled && event.arguments[0].startsWith(commandPrefix)) { // Avoids counting if already disabled
-          counter += Date.now() - prevTimeStamp <= 1000 ? 1 : 0;
-          prevTimeStamp = Date.now();
+        counter += Date.now() - prevTimeStamp <= 1000 ? 1 : 0;
+        prevTimeStamp = Date.now();
 
-      threads[event.thread_id].possibleSpam = counter > threads[event.thread_id].counterLimit;
-      if (threads[event.thread_id].possibleSpam) {
-          threads[event.thread_id].msgIndexEnable = 2;
-          threads[event.thread_id].msgIndexDisable = 3;
-          counter = 0;
-          return true;
-      }
-  }
-  return threads[event.thread_id].isThreadDisabled;
+        threads[event.thread_id].possibleSpam = counter > threads[event.thread_id].counterLimit;
+        if (threads[event.thread_id].possibleSpam) {
+            threads[event.thread_id].msgIndexEnable = 2;
+            threads[event.thread_id].msgIndexDisable = 3;
+            counter = 0;
+            return true;
+        }
+    }
+    return threads[event.thread_id].isThreadDisabled;
 };
 
-exports.run = function(api, event) {
+exports.run = function (api, event) {
     if (event.arguments[0] === api.commandPrefix + commands[0] || threads[event.thread_id].possibleSpam) {
 
         // Command /disable /counter <value>
@@ -67,41 +67,43 @@ exports.run = function(api, event) {
             } else {
                 api.sendMessage(messages[5] + ' ' + event.sender_name, event.thread_id);
             }
-          return false;
+            return false;
 
-    // Command /disable /timer <seconds>
-    } else if (event.arguments_body.startsWith(api.commandPrefix + commands[2])) {
-                var seconds = parseFloat(event.arguments_body.substring(
-                    (api.commandPrefix + commands[2]).length, event.arguments_body.length));
-                        if (isNaN(seconds)) {
-                          api.sendMessage(messages[4] + ' ' + event.sender_name, event.thread_id);
-                      } else {
-                          setTimeout(function(){
-                              threads[event.thread_id].isThreadDisabled = !threads[event.thread_id].isThreadDisabled;
-                          }, seconds * 1000); // Converting seconds to milliseconds
-                            api.sendMessage(messages[5] + ' ' + event.sender_name, event.thread_id);
-                      }
-                      return false;
+            // Command /disable /timer <seconds>
+        } else if (event.arguments_body.startsWith(api.commandPrefix + commands[2])) {
+            var seconds = parseFloat(event.arguments_body.substring(
+                (api.commandPrefix + commands[2]).length, event.arguments_body.length));
+            if (isNaN(seconds)) {
+                api.sendMessage(messages[4] + ' ' + event.sender_name, event.thread_id);
+            } else {
+                setTimeout(function () {
+                    threads[event.thread_id].isThreadDisabled = !threads[event.thread_id].isThreadDisabled;
+                }, seconds * 1000); // Converting seconds to milliseconds
+                api.sendMessage(messages[5] + ' ' + event.sender_name, event.thread_id);
+            }
+            return false;
 
             // Command /disable /default
-            } else if (event.arguments_body === api.commandPrefix + commands[4]) {
-                threads[event.thread_id].counterLimit = 3;
-                return false;
+        } else if (event.arguments_body === api.commandPrefix + commands[4]) {
+            threads[event.thread_id].counterLimit = 3;
+            return false;
 
-                // Main Branch - only executed if no commands were matched
-              } else if (threads[event.thread_id].isThreadDisabled	) {
-                api.sendMessage(messages[msgIndexEnable] + ' ' + event.sender_name, event.thread_id);
-              }
-              else {
-                api.sendMessage(messages[msgIndexDisable], event.thread_id);
-              }
+            // Main Branch - only executed if no commands were matched
+        } else if (threads[event.thread_id].isThreadDisabled) {
+            api.sendMessage(messages[msgIndexEnable] + ' ' + event.sender_name, event.thread_id);
+        } else {
+            api.sendMessage(messages[msgIndexDisable], event.thread_id);
+        }
         threads[event.thread_id].isThreadDisabled = !threads[event.thread_id].isThreadDisabled;
         threads[event.thread_id].possibleSpam = false;
-  }
-  return false;
+    }
+    return false;
 };
 
-exports.help = function(commandPrefix) {
-    return [[commandPrefix + 'disable', 'Disables the platform',
-    'Stops the platform from responding to messages it receives based on message frequency (default 3/sec) or by the command.\nTo renable send the disable command again']];
+exports.help = function (commandPrefix) {
+    return [
+        [commandPrefix + 'disable', 'Disables the platform',
+            'Stops the platform from responding to messages it receives based on message frequency (default 3/sec) or by the command.\nTo renable send the disable command again'
+        ]
+    ];
 };
