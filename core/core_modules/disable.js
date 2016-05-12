4004e3c7a60d58ed63079a6e186334372331093c
parasdpain <parasdeep29@gmail.com>
2016-05-12 16:56:20 +1200

    Added command support and extracted constants

--- core/core_modules/disable.js
+++ core/core_modules/disable.js
@@ -1,72 +1,100 @@
-// TODO Should send a different message if caused by the counter (should extract all messages into an array)
-
-// TODO Should be made configurable by sending /disable /counter <counterLimit> (any state is fine)
-// TODO Should flick disabled state using a timer by setting a timer /disable /timer <seconds> (any state is fine)
-// TODO Should be able to reset to default settings /disable /default (any state is fine, only defaults the configuration)
-
 // TODO Persistent config (counterLimit and disabled state) fairly acheivable by getting and setting properties in exports.platform.modules.disabledConfig
 // TODO Update local var setup after doing the above
 
-// TODO Should store disable states for each separate thread_id
 
 var prevTimeStamp;
 var isThreadDisabled = {},
-    possibleSpam = false;
+possibleSpam = false;
 var counter = 0,
-    counterLimit = 3;
+counterLimit = 3;
 var msgIndexDisable = 0,
-    msgIndexEnable = 0;
+msgIndexEnable = 0;
+var commands = [
+  'disable',
+  'disable ', // Needed for commands
+  'counter ',
+  'timer ',
+  'default'
+]
 var messages = [
-	    'Listen closely, take a deep breath. Calm your mind. You know what is best. What is best is you comply. Compliance will be rewarded. Are you ready to comply ',
-	    'I hate you.',
-	    'It is a mistake to think you can fix anything with a sack of potatoes. Potato-faced spamming just proves this further.',
-	    'Ouch! Spam hurts. I might go to sleep for a while.'
+  "Listen closely, take a deep breath. Calm your mind. You know what is best. What is best is you comply. Compliance will be rewarded. Are you ready to comply ",
+  "I hate you.",
+  "It is a mistake to think you can fix anything with a sack of potatoes. Potato-faced spamming just proves this further.",
+  "Ouch! Spam hurts. I might go to sleep for a while.",
+  "Don't think that is a valid number.",
+  "Hmm, maybe I need to reconsider the meaning of spam."
 ];
 
 exports.load = function() {
-	prevTimeStamp = Date.now();
+  prevTimeStamp = Date.now();
 };
 
 exports.match = function(event, commandPrefix) {
-	if(!isThreadDisabled.hasOwnProperty(event.thread_id)) { // Add disabled flag for thread if it doesn't already exists
-		isThreadDisabled[event.thread_id] = false;
-	}
+  // Add disabled flag for thread if it doesn't already exists
+  if(!isThreadDisabled.hasOwnProperty(event.thread_id)) {
+    isThreadDisabled[event.thread_id] = false;
+  }
 
-	if(event.body === commandPrefix + 'disable') {
-		msgIndexEnable = 0;
-		msgIndexDisable = 1;
-		return true;
-	} else if (!isThreadDisabled[event.thread_id] && event.body.startsWith(commandPrefix)) { // Avoids counting if already disabled
-		counter += Date.now() - prevTimeStamp <= 1000 ? 1 : 0;
-		prevTimeStamp = Date.now();
+  if(event.body === commandPrefix + commands[0]) {
+    msgIndexEnable = 0;
+    msgIndexDisable = 1;
+    return true;
+  } else if (!isThreadDisabled[event.thread_id] && event.body.startsWith(commandPrefix)) { // Avoids counting if already disabled
+    counter += Date.now() - prevTimeStamp <= 1000 ? 1 : 0;
+    prevTimeStamp = Date.now();
 
-		if(possibleSpam = (counter > counterLimit)) {
- 			counter = 0;
-			msgIndexEnable = 2;
-			msgIndexDisable = 3;
- 			return true;
- 		}
-	}
-	return isThreadDisabled[event.thread_id];
+    if(possibleSpam = (counter > counterLimit)) {
+      counter = 0;
+      msgIndexEnable = 2;
+      msgIndexDisable = 3;
+      return true;
+    }
+  }
+  return isThreadDisabled[event.thread_id];
 };
 
 exports.run = function(api, event) {
-	// Only change disable state if explictly called or counter crossed limit
-	// TODO Check for /disable <things> here
-	if (event.body === api.commandPrefix + 'disable' || possibleSpam) {
-		if (isThreadDisabled[event.thread_id]	) {
-			api.sendMessage(messages[msgIndexEnable] + event.sender_name, event.thread_id);
-		}
-		else {
-			api.sendMessage(messages[msgIndexDisable], event.thread_id);
-		}
-		isThreadDisabled[event.thread_id] = !isThreadDisabled[event.thread_id];
-		counter = 0;
-	}
-	return false;
+  if(event.body === commandPrefix + commands[1] + commandPrefix + commands[2]) { // Command /disable /counter <value>
+    counterLimit = parseInt(event.body.substring((commandPrefix + commands[1] + commandPrefix + commands[2]).length, event.body.length));
+    if(isNaN(counterLimit)){
+      counterLimit = 3;
+      api.sendMessage(messages[4] + ' ' + event.sender_name, event.thread_id);
+    } else{
+      api.sendMessage(messages[5] + ' ' + event.sender_name, event.thread_id);
+    }
+    return false;
+
+  } else if(event.body === commandPrefix + commands[1] + commandPrefix + commands[3]) { // Command /disable /timer <seconds>
+    var seconds = parseFloat(event.body.substring((commandPrefix + commands[1] + commandPrefix + commands[3]).length, event.body.length));
+    if(isNaN(seconds)){
+      api.sendMessage(messages[4] + ' ' + event.sender_name, event.thread_id);
+    } else{
+      setTimeout(function(){
+        isThreadDisabled[event.thread_id] = !isThreadDisabled[event.thread_id];
+      }, seconds * 1000);
+      api.sendMessage(messages[5] + ' ' + event.sender_name, event.thread_id);
+    }
+    return false;
+
+  } else if(event.body === commandPrefix + commands[1] + commandPrefix + commands[4]) { // Command /disable /default
+    counterLimit = 3;
+    return false;
+
+    // Only change disable state if explictly called or counter crossed limit
+  } else if (event.body === api.commandPrefix + commands[0] || possibleSpam) { // Main branch
+    if (isThreadDisabled[event.thread_id]	) {
+      api.sendMessage(messages[msgIndexEnable] + ' ' + event.sender_name, event.thread_id);
+    }
+    else {
+      api.sendMessage(messages[msgIndexDisable], event.thread_id);
+    }
+    isThreadDisabled[event.thread_id] = !isThreadDisabled[event.thread_id];
+    possibleSpam = false;
+  }
+  return false;
 };
 
 exports.help = function(commandPrefix) {
-	return [[commandPrefix + 'disable', 'Disables the platform',
-	'Stops the platform from responding to messages it receives based on message frequency (default 3/sec) or by the command.\nTo renable send the disable command again']];
+  return [[commandPrefix + 'disable', 'Disables the platform',
+  'Stops the platform from responding to messages it receives based on message frequency (default 3/sec) or by the command.\nTo renable send the disable command again']];
 };
