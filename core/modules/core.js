7b77dc054a447d37eea8551cdd626d24a4b7d1bc
Matthew <mrk45@uclive.ac.nz>
2016-05-15 20:37:59 +1200

    Spam prevention

--- core/modules/core.js
+++ core/modules/core.js
@@ -2,11 +2,11 @@
  * Provides helper functions for handling user and system modules.
  *
  * Written By:
- * 		Matthew Knox
+ *         Matthew Knox
  *
  * License:
- *		MIT License. All code unless otherwise specified is
- *		Copyright (c) Matthew Knox and Contributors 2015.
+ *        MIT License. All code unless otherwise specified is
+ *        Copyright (c) Matthew Knox and Contributors 2015.
  */
 
 var path            = require('path'),
@@ -27,10 +27,15 @@ exports.loadCoreModule = function(platform, module) {
         m = index !== -1 ? require.reload(fp) : require.once(fp);
     m.platform = exports.platform;
     m.name = module.split('.')[0];
-    if (m.load) {
-        m.load.call(platform);
-    }
     return m;
+};
+
+exports.loadingComplete = function (modules) {
+    for (var i = 0; i < modules.length; i++) {
+        if (modules[i].load) {
+            modules[i].load.call(modules[i].platform);
+        }
+    }
 };
 
 exports.unloadCoreModule = function(mod) {
