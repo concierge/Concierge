1cc309576a365321e19a65992115d86b1b8979bb
Phillip Smith <ps@phillipadsmith.com>
2016-07-01 20:33:54 -0700

    Fix Coadacy issues

--- core/core_integrations/telegram.js
+++ core/core_integrations/telegram.js
@@ -4,8 +4,8 @@ var bot = null,
     TelegramBot = require.safe('node-telegram-bot-api');
 
 
-var messagePadder = function( str, length ) {
-    var padding = ( new Array( Math.max( length - str.length + 1, 0 ) ) ).join( " " );
+var messagePadder = function(str, length) {
+    var padding = ( new Array( Math.max( length - str.length + 1, 0 ) ) ).join( ' ' );
     return str + padding;
 };
 
@@ -13,7 +13,7 @@ var messageSplitter = function(message) {
     var chunks = message.match(/[^]{1,4096}/g);
     // This is kinda' crazy too. Basically, Telegram delivers smaller length
     // messages first, thus it helps to keep them the same length
-    for (var i=0; i < chunks.length; i++) {
+    for (var i = 0; i < chunks.length; i++) {
         if (chunks[i].length < 4096) {
             var pad_needed = 4096 - chunks[i].length;
             var padding = messagePadder(chunks[i], pad_needed);
@@ -27,7 +27,7 @@ var sendMessage = function(message, thread, callback) {
     bot.sendMessage(thread, message);
     if (callback) {
         // Need to use this silliness because Telegram delivers
-        // messages out of order
+        // messages out of order. Maybe look at async?
         setTimeout(callback(), 1000);
     }
 };
