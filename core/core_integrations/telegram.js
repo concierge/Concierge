a3474d9b41422b1b131c3d0db4a823b952ece6c9
Phillip Smith <ps@phillipadsmith.com>
2016-07-03 18:12:44 -0700

    Feature/telegram support 0.23.3 master (#185)
    * Ignore OSX .DS_Store
    * Remove dependency on monkey patch in Telegram
    
    * Remove monkey patch
    
    * Fix .stop() method

--- core/core_integrations/telegram.js
+++ core/core_integrations/telegram.js
@@ -1,15 +1,7 @@
-var options = {
-    // Used by the monkey patch to node-telegram-bot-api
-    stopPolling: true
-},
-    TelegramBot = require.safe('node-telegram-bot-api'),
-    bot = null,
+var bot = null,
+    api = null,
     shim = require('../shim.js'),
-    api = null;
-
-// Adds the missing stopPolling() method
-// https://github.com/yagop/node-telegram-bot-api/pull/51#issuecomment-217395990
-require.safe('monkey-patches-node-telegram-bot-api')(TelegramBot, options);
+    TelegramBot = require.safe('node-telegram-bot-api');
 
 var sendMessage = function(message, thread, opts) {
     bot.sendMessage(thread, message, opts);
@@ -38,6 +30,11 @@ exports.start = function(callback) {
 
 exports.stop = function() {
     console.debug('Telegram -> start shutdown');
+    // Ensure that there's no endless loop because of an incorrect offset value
     exports.config.offset = bot._polling.offset + 1;
-    bot.stopPolling();
+    // No stopPolling() method, so we set the abort to true & that seems to work
+    if (bot._polling) {
+        bot._polling.abort = true;
+    }
+    this._polling = null;
 };
