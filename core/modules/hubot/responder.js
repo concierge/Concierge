697e4e701bab23ae9ee07c7a48df71c74c87d3f8
Matthew Knox <mrkno@users.noreply.github.com>
2016-08-28 18:26:27 +1200

    Add some missing hubot api calls (#205)
    * Add missing functionality from Hubot API

--- core/modules/hubot/responder.js
+++ core/modules/hubot/responder.js
@@ -27,29 +27,38 @@ var Responder = function (api, event, match, message) {
     this.event = event;
     this.match = match;
     this.message = message;
+    this.envelope = message;
 };
 
 Responder.prototype.random = function (arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 };
 
-Responder.prototype.send = function () {
-    for (var i = 0; i < arguments.length; i++) {
-        switch (getType(arguments[i])) {
+Responder.prototype._internalSend = function (thread_id, data) {
+    for (var i = 0; i < data.length; i++) {
+        switch (getType(data[i])) {
         case 'message':
-            this.api.sendMessage(arguments[i], this.event.thread_id);
+            this.api.sendMessage(data[i], thread_id);
             break;
         case 'url':
-            this.api.sendUrl(arguments[i], this.event.thread_id);
+            this.api.sendUrl(data[i], thread_id);
             break;
         case 'image':
-            this.api.sendImage('url', arguments[i], '', this.event.thread_id);
+            this.api.sendImage('url', data[i], '', thread_id);
             break;
         case 'file':
-            this.api.sendFile('file', arguments[i], '', this.event.thread_id);
+            this.api.sendFile('file', data[i], '', thread_id);
             break;
         }
     }
+};
+
+Responder.prototype.send = function () {
+    this._internalSend(this.event.thread_id, arguments);
+};
+
+Responder.prototype.messageRoom = function (room, strings) {
+    this._internalSend(room, strings);
 };
 
 Responder.prototype.emote = Responder.prototype.send;
