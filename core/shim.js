c47b84d8bf37b322ce3f46349274c4d756ce0e81
Matthew Knox <mrk45@uclive.ac.nz>
2016-09-03 15:16:44 +1200

    Tidied shim

--- core/shim.js
+++ core/shim.js
@@ -1,18 +1,19 @@
-var sendMessageToMultiple = function (message, threads) {
-        var apis = exports.current.getIntegrationApis();
-        for (var integ in threads) {
+let scopedHttpClient = require('scoped-http-client'),
+    sendMessageToMultiple = (message, threads) => {
+        let apis = exports.current.getIntegrationApis();
+        for (let integ in threads) {
             if (!threads.hasOwnProperty(integ)) {
                 continue;
             }
-            var intThreads = threads[integ];
-            for (var i = 0; i < intThreads.length; i++) {
+            let intThreads = threads[integ];
+            for (let i = 0; i < intThreads.length; i++) {
                 apis[integ].sendMessage(message, intThreads[i]);
             }
         }
     },
 
-    _loopbackWrapper = function (origionalSend, api) {
-        return function (data, thread) {
+    _loopbackWrapper = (origionalSend, api) => {
+        return (data, thread) => {
             origionalSend(data, thread);
             if (exports.current && exports.current.allowLoopback) {
                 let newEvent = exports.createEvent(thread, -1, 'Bot', data);
@@ -22,113 +23,116 @@ var sendMessageToMultiple = function (message, threads) {
         };
     };
 
-exports.createIntegration = function (platform) {
-    if (!platform.sendMessage) {
-        platform.sendMessage = function() {
-            throw new Error($$`What kind of shit platform is this that doesn\'t even support sending messages?`);
-        };
+let IntegrationApi = module.exports = class {
+    constructor(prefix) {
+        this.commandPrefix = prefix || '/';
+        this.sendMessage = _loopbackWrapper(this.sendMessage, this);
+        this.sendUrl = _loopbackWrapper(this.sendUrl, this);
     }
-    platform.sendMessage = _loopbackWrapper(platform.sendMessage, platform);
 
-    if (!platform.sendUrl) {
-        platform.sendUrl = function(url, thread) {
-            platform.sendMessage(url, thread); // fallback to sending a message
-        };
+    sendMessage() {
+        throw new Error($$`What kind of shit platform is this that doesn\'t even support sending messages?`);
     }
-    platform.sendUrl = _loopbackWrapper(platform.sendUrl, platform);
 
-    if (!platform.sendImage) {
-        platform.sendImage = function(type, image, description, thread) {
-            switch(type) {
+    sendUrl(url, thread) {
+        this.sendMessage(url, thread); // fallback to sending a message
+    }
+
+    sendImage(type, image, description, thread) {
+        switch(type) {
             case 'url': // fallback to sending a url
-                platform.sendMessage(description, thread);
-                platform.sendUrl(image, thread);
+                this.sendMessage(description, thread);
+                this.sendUrl(image, thread);
                 break;
             case 'file': // fallback to sending a file
-                platform.sendFile(type, image, description, thread);
+                this.sendFile(type, image, description, thread);
                 break;
             default: // fallback to sending a message
-                platform.sendMessage(description, thread);
-                platform.sendMessage($$`I also have something to send you but cant seem to do so...`, thread);
+                this.sendMessage(description, thread);
+                this.sendMessage($$`I also have something to send you but cant seem to do so...`, thread);
                 break;
-            }
-        };
+        }
     }
 
-    if (!platform.sendFile) {
-        platform.sendFile = function(type, file, description, thread) {
-            platform.sendMessage(description, thread);
-            switch(type) {
+    sendFile(type, file, description, thread) {
+        this.sendMessage(description, thread);
+        switch(type) {
             case 'url': // fallback to sending a url
-                platform.sendUrl(file, thread);
+                this.sendUrl(file, thread);
                 break;
             case 'file': // fallback to sending a message
-                platform.sendMessage($$`I have a file to send you but cant seem to do so...`, thread);
+                this.sendMessage($$`I have a file to send you but cant seem to do so...`, thread);
                 break;
             default: // fallback to sending a message
-                platform.sendMessage($$`I have something to send you but cant seem to do so...`, thread);
+                this.sendMessage($$`I have something to send you but cant seem to do so...`, thread);
                 break;
-            }
-        };
-    }
-
-    if (!platform.sendTyping) {
-        platform.sendTyping = function(thread) {
-            platform.sendMessage($$`Working on it...`, thread); // fallback to sending a message
-        };
-    }
-
-    if (!platform.setTitle) {
-        platform.setTitle = function(title, thread) { // fallback to sending a message
-            platform.sendMessage($$`If I could set the title of this chat I would set it to "${title}"`, thread);
-        };
-    }
-
-    if (!platform.sendPrivateMessage) {
-        platform.sendPrivateMessage = function (message, thread) {
-            platform.sendMessage(message, thread);
-        };
-    }
-
-    if (!platform.getUsers) {
-        platform.getUsers = function () {
-            return {};
-        };
-    }
-
-    if (!platform.commandPrefix) {
-        if (platform.config && platform.config.commandPrefix) {
-            platform.commandPrefix = platform.config.commandPrefix;
-        }
-        else {
-            platform.commandPrefix = '/';
         }
     }
 
-    platform.sendMessageToMultiple = sendMessageToMultiple;
-    platform.http = require('scoped-http-client').create;
-    platform.random = (arr) => {
+    sendTyping(thread) {
+        this.sendMessage($$`Working on it...`, thread); // fallback to sending a message
+    }
+
+    setTitle(title, thread) { // fallback to sending a message
+        this.sendMessage($$`If I could set the title of this chat I would set it to "${title}"`, thread);
+    }
+
+    sendPrivateMessage(message, thread) {
+        this.sendMessage(message, thread);
+    }
+
+    sendMessageToMultiple(message, threads) {
+        sendMessageToMultiple(message, threads);
+    }
+
+    getUsers() {
+        return {};
+    }
+
+    random(arr) {
         return arr[Math.floor(Math.random() * arr.length)];
-    };
-
-    return platform;
-};
-
-exports.createEvent = function(thread, senderId, senderName, message) {
-    var event = {
-        thread_id: thread,
-        sender_id: senderId,
-        sender_name: senderName + '', // Accept sender_name  = null as a literal
-        body: message,
-        event_source: null
-    };
-    event.arguments = event.body.match(/(?:[^\s"]+|"[^"]*")+/g);
-    if (event.arguments === null) {
-        event.arguments = [''];
     }
-    event.arguments_body = event.body.substr(event.arguments[0].length + 1);
-    for (var i = 0; i < event.arguments.length; i++) {
-        event.arguments[i] = event.arguments[i].replace(/(^["])|(["]$)/g, '');
+
+    http() {
+        return scopedHttpClient.create.apply(this, arguments);
     }
-    return event;
+
+    _getBaseClassProperties() {
+        let items = Object.getOwnPropertyNames(IntegrationApi.prototype);
+        items.splice(items.indexOf('constructor'), 1);
+        return items.concat(Object.keys(this));
+    }
+
+    static createIntegration(implementation) {
+        let integ = new IntegrationApi(implementation),
+            properties = integ._getBaseClassProperties();
+        for (let property of properties) {
+            if (implementation.hasOwnProperty(property)) {
+                integ[property] = implementation[property];
+            }
+        }
+        if (implementation.config && implementation.config.commandPrefix) {
+            integ.commandPrefix = implementation.config.commandPrefix;
+        }
+        return integ;
+    }
+
+    static createEvent(thread, senderId, senderName, message) {
+        let event = {
+            thread_id: thread,
+            sender_id: senderId,
+            sender_name: senderName + '', // Accept sender_name  = null as a literal
+            body: message,
+            event_source: null
+        };
+        event.arguments = event.body.match(/(?:[^\s"]+|"[^"]*")+/g);
+        if (event.arguments === null) {
+            event.arguments = [''];
+        }
+        event.arguments_body = event.body.substr(event.arguments[0].length + 1);
+        for (let i = 0; i < event.arguments.length; i++) {
+            event.arguments[i] = event.arguments[i].replace(/(^["])|(["]$)/g, '');
+        }
+        return event;
+    }
 };
