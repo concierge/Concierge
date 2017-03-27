eb137244f56bd254dd9e7b84b87faf2c7b4f763c
Matthew Knox <mrk45@uclive.ac.nz>
2017-03-28 11:08:30 +1300

    Add artificial delay in attempt to fix CI

--- test/helpers/client.js
+++ test/helpers/client.js
@@ -1,73 +1,75 @@
-var fs = require('fs'),
-    WebSocket = require('ws'),
-    callback = null,
-    stopCallback = null,
-    messageCallback = null,
-    threadId = 1,
-    ws = null,
-    config = null,
-    port = 49886,
+const fs = require('fs'),
+    WebSocket = require('ws');
 
-    responsdWithCallback = function(data) {
-        if (messageCallback && data.thread_id === threadId - 1) {
-            messageCallback.callback(data, messageCallback.done);
+class Client {
+    constructor() {
+        this.callback = null;
+        this.stopCallback = null;
+        this.messageCallback = null;
+        this.threadId = 1;
+        this.ws = null;
+        this.config = null;
+        try {
+            this.config = require('./config.json');
         }
-    },
+        catch (e) {
+            console.info('config not found, continue with defaults');
+        }
+        this.port = (((this.config || {}).output || {}).grunt || {}).port || 49886;
+    }
 
-    Client = function(cb) {
-        callback = cb;
+    _respondWithCallback (data) {
+        if (this.messageCallback && data.thread_id === this.threadId - 1) {
+            this.messageCallback.callback(data, this.messageCallback.done);
+        }
+    }
 
-        ws.on('open', function() {
-            if (callback) {
-                callback();
-            }
-        });
+    _onOpen () {
+        if (this.callback) {
+            this.callback();
+        }
+    }
 
-        ws.on('close', function() {
-            if (stopCallback) {
-                stopCallback();
-            }
-        });
+    _onClose () {
+        if (this.stopCallback) {
+            this.stopCallback();
+        }
+    }
 
-        ws.on('message', function(data) {
-            responsdWithCallback(JSON.parse(data));
-        });
+    _onMessage (data) {
+        this._respondWithCallback(JSON.parse(data));
+    }
 
-    };
+    sendMessage (message) {
+        this.ws.send(JSON.stringify({
+            content: message,
+            thread_id: this.threadId++
+        }));
+    }
 
-Client.prototype.sendMessage = function(message) {
-    ws.send(JSON.stringify({
-        content: message,
-        thread_id: threadId++
-    }));
-};
+    receiveMessage (cb, done) {
+        this.messageCallback = {
+            callback: cb,
+            done: done
+        };
+    }
 
-Client.prototype.receiveMessage = function(cb, done) {
-    messageCallback = {
-        callback: cb,
-        done: done
-    };
-};
+    start (cb) {
+        this.callback = cb;
+        setTimeout(() => {
+            this.ws = new WebSocket(`ws://localhost:${this.port}`);
+            this.ws.on('open', this._onOpen.bind(this));
+            this.ws.on('close', this._onClose.bind(this));
+            this.ws.on('message', this._onMessage.bind(this));
+        }, 5000); // give the socket a chance to start
+    }
 
-Client.prototype.shutdown = function(cb) {
-    ws.send(JSON.stringify({
-        content: '/shutdown'
-    }));
-    stopCallback = cb;
-};
-
-try {
-    var file = fs.readFileSync('config.json', 'utf8');
-    config = JSON.parse(file);
+    shutdown (cb) {
+        this.ws.send(JSON.stringify({
+            content: '/shutdown'
+        }));
+        this.stopCallback = cb;
+    }
 }
-catch (e) {
-    console.info('config not found, continue with defaults');
-}
-
-if (config !== null && config.output && config.output.grunt && config.output.grunt.port) {
-    port = config.output.grunt.port;
-}
-
-ws = new WebSocket('ws://localhost:' + port);
 
 module.exports = Client;
