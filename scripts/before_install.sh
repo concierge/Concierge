790f24b7d5472ebf5368bd4b7b780997d4cbbae9
Dion Woolley <woolley.dion@gmail.com>
2017-04-17 10:51:58 +1200

    Move deploy to seperate repo

--- scripts/before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# update instance
-apt-get update
-
-# add nodejs to yum
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-apt-get install -y nodejs tmux
