7ba1cd1e729ceef3ca031d359f314443eb22bfe7
Matthew <mrk45@uclive.ac.nz>
2017-09-26 21:58:19 +1300

    Add the ability to run Concierge under Electron node. This is a monkey patch to spawn (and hence fork) to remove an environment variable which removes the ability to create windows on a forked process.

--- core/startup/startup.js
+++ core/startup/startup.js
@@ -17,9 +17,24 @@
 
 'use strict';
 
-const fork = require('child_process').fork,
-    path = require('path');
+const cp = require('child_process'),
+	fork = cp.fork,
+    path = require('path'),
+	origSpawn = cp.ChildProcess.prototype.spawn;
 
+cp.ChildProcess.prototype.spawn = function (...args) {
+	const arg0 = args[0];
+	if (arg0 && arg0.envPairs) {
+		const entries = arg0.envPairs.filter(i => i.startsWith('ELECTRON_RUN_AS_NODE'));
+		for (let entry of entries) {
+			const index = arg0.envPairs.indexOf(entry);
+			console.log(`Removing ${entry} at ${index}`);
+			arg0.envPairs.splice(index, 1);
+		}
+	}
+	origSpawn.apply(this, args);
+};
+	
 global.StatusFlag = {
     Unknown: 1,
     NotStarted: 2,
